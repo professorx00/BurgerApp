0fb3a572eb1a434ebea96b42bfd1b6ec50ac538e
Dave Ward <professorx00@gmail.com>
2019-10-03 15:53:56 -0400

    Update to use Heroku Env Files

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("burgerTown", "root", "root", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+var sequelize = new Sequelize(process.env.JAWSDB_URL);
 
 // Exports the connection for other files to use
 module.exports = sequelize;
